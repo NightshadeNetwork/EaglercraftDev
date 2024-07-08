1b9ec58d291270b9b5df330edc56b2d83e817707
ServerNootNoot <servernootnoot@gmail.com>
2024-07-08 01:07:28 -0400

    Misc

---
+++ 1.8/injectUserscript.js
@@ -0,0 +1,14 @@
+function injectMobileUserscript() {
+    const script = document.createElement('script');
+    script.src = '/1.8/eaglermobile.user.js';
+    script.type = 'text/javascript';
+    document.head.appendChild(script);
+}
+
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+
+if (isMobileDevice()) {
+    injectMobileUserscript();
+}
